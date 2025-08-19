c3c602573c07e1bacdc39c24b48b0a0f25b1fa7f
workspaceuser <workspaceuser@plot.ly>
2025-08-19 12:09:02 +0000

    One Click Deploy - 1755605342507

---
+++ app.py
@@ -0,0 +1,88 @@
+from dash import Dash, dcc, html, callback, Input, Output, State
+import dash_design_kit as ddk
+import plotly.express as px
+from theme import my_theme
+
+app = Dash(__name__)
+
+
+def get_data():
+    # Retrieve data in Python.
+    df = px.data.stocks()
+
+    # Post-process data in Python.
+    df.fillna(0)
+    return df
+
+
+def run_model(df, stock):
+    return df.drop(["date"], axis=1).rolling(10).mean()
+
+
+app.layout = ddk.App(
+    [
+        ddk.Header(
+            [
+                ddk.Logo(src=app.get_asset_url("logo.svg")),
+                ddk.Title("Monthly Analysis"),
+            ]
+        ),
+        dcc.Dropdown(
+            options=["GOOG", "AAPL", "AMZN"],
+            id="title-dropdown",
+            value="GOOG",
+        ),
+        ddk.Card(
+            children=[
+                ddk.Graph(id="graph-1", style={"height": "65vh"}),
+            ],
+            width=100,
+        ),
+        ddk.Card(
+            width=50,
+            children=ddk.Graph(
+                id="graph-2",
+                figure=px.line(
+                    get_data(),
+                    x="date",
+                    y=["AMZN", "GOOG"],
+                    title="Stock Prices",
+                )
+            ),
+        ),
+        ddk.Card(
+            width=50,
+            children=ddk.Graph(
+                id="graph-3",
+                figure=px.bar(
+                    get_data(),
+                    x="date",
+                    y=["AAPL", "MSFT"],
+                    barmode="stack",
+                    title="Stock Prices"
+                )
+            ),
+        ),
+    ],
+    show_editor=True, # Show DDK editor button.
+    theme=my_theme,
+)
+
+
+@callback(
+    Output("graph-1", "figure"),
+    Input("title-dropdown", "value")
+)
+def update_graph(dropdown_value):
+    df = get_data()
+    model = run_model(df, dropdown_value)
+
+    return px.line(
+        model,
+        y=dropdown_value,
+    )
+
+
+server = app.server
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ theme.py
@@ -0,0 +1,112 @@
+my_theme = {
+    "accent":"#1f78b4",
+    "accent_positive":"#017500",
+    "accent_negative":"#C20000",
+    "background_content":"#F9F9F9",
+    "background_page":"#F2F2F2",
+    "body_text":"#606060",
+    "border":"#e2e2e2",
+    "border_style":{
+        "name":"underlined",
+        "borderWidth":"0px 0px 1px 0px",
+        "borderStyle":"solid",
+        "borderRadius":0
+    },
+    "button_border":{
+        "width":"1px",
+        "color":"#fa4f56",
+        "radius":"0px"
+    },
+    "button_capitalization":"uppercase",
+    "button_text":"#1f78b4",
+    "button_background_color":"#F9F9F9",
+    "control_border":{
+        "width":"0px 0px 1px 0px",
+        "color":"#e2e2e2",
+        "radius":"0px"
+    },
+    "control_background_color":"#F9F9F9",
+    "control_text":"#606060",
+    "card_margin":"15px",
+    "card_padding":"5px",
+    "card_border":{
+        "width":"0px",
+        "style":"solid",
+        "color":"#e2e2e2",
+        "radius":"0px"
+    },
+    "card_background_color":"#F9F9F9",
+    "card_box_shadow":"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
+    "card_outline":{
+        "width":"0px",
+        "style":"solid",
+        "color":"#e2e2e2"
+    },
+    "card_header_margin":"0px",
+    "card_header_padding":"10px",
+    "card_header_border":{
+        "width":"0px 0px 1px 0px",
+        "style":"solid",
+        "color":"#e2e2e2",
+        "radius":"0px"
+    },
+    "card_header_background_color":"#F9F9F9",
+    "card_header_box_shadow":"0px 0px 0px rgba(0,0,0,0)",
+    "breakpoint_font":"1200px",
+    "breakpoint_stack_blocks":"700px",
+    "colorway":[
+        "#119dff",
+        "#66c2a5",
+        "#fc8d62",
+        "#e78ac3",
+        "#a6d854",
+        "#ffd92f",
+        "#e5c494",
+        "#b3b3b3"
+    ],
+    "colorscale":[
+        "#1f78b4",
+        "#4786bc",
+        "#6394c5",
+        "#7ba3cd",
+        "#92b1d5",
+        "#a9c0de",
+        "#bed0e6",
+        "#d4dfee",
+        "#eaeff7",
+        "#ffffff"
+    ],
+    "dbc_primary":"#1f78b4",
+    "dbc_secondary":"#7c7c7c",
+    "dbc_info":"#009AC7",
+    "dbc_gray":"#adb5bd",
+    "dbc_success":"#017500",
+    "dbc_warning":"#F9F871",
+    "dbc_danger":"#C20000",
+    "font_family":"Open Sans",
+    "font_family_header":"Open Sans",
+    "font_family_headings":"Open Sans",
+    "font_size":"17px",
+    "font_size_smaller_screen":"15px",
+    "font_size_header":"24px",
+    "title_capitalization":"uppercase",
+    "header_content_alignment":"spread",
+    "header_margin":"0px 0px 15px 0px",
+    "header_padding":"0px",
+    "header_border":{
+        "width":"0px 0px 1px",
+        "style":"solid",
+        "color":"#e2e2e2",
+        "radius":"0px"
+    },
+    "header_background_color":"#F9F9F9",
+    "header_box_shadow":"0px 1px 3px rgba(0,0,0,0.12), 0px 1px 2px rgba(0,0,0,0.24)",
+    "header_text":"#606060",
+    "heading_text":"#606060",
+    "text":"#606060",
+    "report_background_content":"#FAFBFC",
+    "report_background_page":"white",
+    "report_text":"black",
+    "report_font_family":"Computer Modern",
+    "report_font_size":"12px"
+}
